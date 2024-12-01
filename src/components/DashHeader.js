13143fe1e2d611f77982a3bb1a55deaa66287302
Amalu Ekwelie <67154046+Prim3timer@users.noreply.github.com>
2024-11-30 21:29:37 -0500

    testing github connection

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -131,7 +131,7 @@ const DashHeader = () => {
 
             <header className="dash-header">
                 <div className={`dash-header__container ${dashClass}`}>
-                    <Link to="/dash">
+                    <Link to="/dash/notes">
                         <h1 className="dash-header__title">techNotes</h1>
                     </Link>
                     <nav className="dash-header__nav">
--- src/features/notes/Note.js
+++ src/features/notes/Note.js
@@ -20,6 +20,7 @@ const Note = ({ noteId }) => {
 
         const updated = new Date(note.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
 
+
         const handleEdit = () => navigate(`/dash/notes/${noteId}`)
 
         return (
@@ -33,7 +34,7 @@ const Note = ({ noteId }) => {
                 <td className="table__cell note__created">{created}</td>
                 <td className="table__cell note__updated">{updated}</td>
                 <td className="table__cell note__title">{note.title}</td>
-                <td className="table__cell note__username">{note.username}</td>
+                <td className="table__cell note__username">{noteId}</td>
 
                 <td className="table__cell">
                     <button
